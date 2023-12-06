53c8dafa76c1de5135354af532870eb57a52c449
Kidus <kingkidy50@gmail.com>
2023-12-06 05:50:41 -0800

    debug

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/pyhton3
 
 def uniq_add(my_list=[]):
+    """
+    Add's a function that add a unique list
+    """
     new_list = set(my_list)
     sum = 0
-    for new_num in new_list:
-        sum += new_num
+    for newnum in new_list:
+        sum += newnum
     return sum
