271d808b168ab81ca971b085781db7889f7fafa4
Kidus <kingkidy50@gmail.com>
2023-12-06 17:27:48 -0800

    new debug with timespec

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -7,23 +7,23 @@ struct timespec;
 
 /**
  * print_python_bytes - This pritns info abt the py byts
- * @obj: Pyobject This is a py byts object
+ * @p: Pyobject This is a py byts object
  */
 
-void print_python_bytes(PyObject *obj)
+void print_python_bytes(PyObject *p)
 {
 	char *byte_str;
 	long int byte_size, idx, byte_limit;
 
 	printf("[.] bytes object info\n");
-	if (!PyBytes_Check(obj))
+	if (!PyBytes_Check(p))
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 
-	byte_size = ((PyVarObject *)(obj))->ob_size;
-	byte_str = ((PyBytesObject *)obj)->ob_sval;
+	byte_size = ((PyVarObject *)(p))->ob_size;
+	byte_str = ((PyBytesObject *)p)->ob_sval;
 
 	printf("  size: %ld\n", byte_size);
 	printf("  trying string: %s\n", byte_str);
@@ -46,10 +46,10 @@ void print_python_bytes(PyObject *obj)
 
 /**
  * print_python_list - This also Pritn info abt the py list
- * @obj: Pyobject this the list of the obj
+ * @p: Pyobject this the list of the obj
  */
 
-void print_python_list(PyObject *obj)
+void print_python_list(PyObject *p)
 {
 	long int list_size, idx;
 	PyListObject *list;
@@ -57,8 +57,8 @@ void print_python_list(PyObject *obj)
 
 	printf("[*] Python list info\n");
 
-	list_size = ((PyVarObject *)(obj))->ob_size;
-	list = (PyListObject *)obj;
+	list_size = ((PyVarObject *)(p))->ob_size;
+	list = (PyListObject *)p;
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", list_size);
@@ -66,7 +66,7 @@ void print_python_list(PyObject *obj)
 
 	for (idx = 0; idx < list_size; idx++)
 	{
-		item = ((PyListObject *)obj)->ob_item[idx];
+		item = ((PyListObject *)p)->ob_item[idx];
 		printf("Element %ld: %s\n", idx, ((item)->ob_type)->tp_name);
 		if (PyBytes_Check(item))
 			print_python_bytes(item);
