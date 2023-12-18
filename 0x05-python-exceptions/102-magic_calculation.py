8d7dc7bf0521734fe01f19ce845db152f125f257
Kidus <kingkidy50@gmail.com>
2023-12-18 08:09:46 -0800

    advancedebug

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -2,7 +2,7 @@
 
 def magic_calculation(a, b):
     totals = 0
-    for m in range(1, 4):
+    for m in range(1, 3):
         try:
             if (m > a):
                 raise (Exception('Too far'))
