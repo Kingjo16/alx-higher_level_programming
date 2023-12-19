8a739162d548204be3c0b6cd79d1f6eb5f77c10a
Kidus <kingkidy50@gmail.com>
2023-12-19 03:39:16 -0800

    mando

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
+"""This Define a class Square."""
 
 
 class Square:
+     """A class representing a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
 
     def __init__(self, size):
+        """Initialize a square with a given size.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
