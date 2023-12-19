e2012df905721d32f1f15cc8c517ff06e321872d
Kidus <kingkidy50@gmail.com>
2023-12-19 03:45:58 -0800

    new line

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,7 +4,10 @@
 
 class Square:
      """A class representing a sq"""
-
     def __init__(self, size):
-        """Initialize a square with a given siz"""
+        """Initialize a square with a given siz
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
         self.__size = size
