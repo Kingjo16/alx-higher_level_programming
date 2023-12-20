d411f2eb8361c73d3f1ea9b8ea69dc79a3e2076e
Kidus <kingkidy50@gmail.com>
2023-12-20 02:13:29 -0800

    deone debuging

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,16 +1,56 @@
 #!/usr/bin/python3
+"""THis define a square"""
 
 
 class Square:
+    """
+    represrnts a square.
+
+    Attributes:
+        __size (int): The size of the sq.
+    """
+
     def __init__(self, size=0):
+        """
+        intialize a new sq class.
+        Args:
+            size (int, optional): The size of the sq.
+        Raises:
+            TypeError: If size is not an int.
+            ValueError: If size is less than 0.
+        """
         self.size = size
+
     def area(self):
+        """
+        clac the area.
+
+        Return:
+            int: The area of the square.
+        """
         return (self.__size) ** 2
+
     @property
     def size(self):
+        """
+        the size attribute.
+
+        Returns:
+            int: the size of the sq.
+        """
         return self.__size
+
     @size.setter
     def size(self, value):
+        """
+        setter meth for the si att.
+
+        Args:
+            value (int): the size set
+        Raises:
+            TypeError: Value not an int.
+            ValueError: If value is less than 0.
+        """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         else:
