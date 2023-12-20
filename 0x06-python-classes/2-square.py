e985d7de64571c5efa38d92f9e535506b57f58bb
Kidus <kingkidy50@gmail.com>
2023-12-20 02:21:51 -0800

    new number 2 addition

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -17,10 +17,8 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
-        if type(size) is not int:
-             raise TypeError("size must be an integer")
-         else:
-             if size < 0:
-                 raise ValueError("size must be >= 0")
-             else:
-                 self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        else size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
