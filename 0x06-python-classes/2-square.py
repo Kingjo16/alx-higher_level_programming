6d944b93906a658895305821114ee7727359dea5
Kidus <kingkidy50@gmail.com>
2023-12-20 02:30:17 -0800

    new try module

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This defines a square"""
+
+"""Module to define the Square class."""
 
 
 class Square:
@@ -22,6 +23,6 @@ class Square:
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        else size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
