74e6869dbea4171cb8412513382d2b3b67da89b0
Kidus <kingkidy50@gmail.com>
2024-01-02 02:13:51 -0800

    addition for int

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
+"""
+This is the 0-add_string module.
+
+This Module 0-add_string is used for adding two int(a, b).
+"""
+
 
 def add_integer(a, b=98):
+    """
+    Add two integer.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
