11934958899cf43096276e512cce9de49855f5cf
Kidus <kingkidy50@gmail.com>
2024-01-15 05:18:16 -0800

    new addition

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import json
+import csv
+import turtle
+
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        located_file = cls.__name__ + ".json"
+        with open(located_file, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                dicts = [o.to_dictionary() for m in list_objs]
+                jsonfile.write(Base.to_json_string(dicts))
+
+
