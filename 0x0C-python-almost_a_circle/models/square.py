8cea044c5fab072682c3af4ce9163eb8ab4a7ac4
Kidus <kingkidy50@gmail.com>
2024-01-19 01:49:44 -0800

    new addition

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
+"""Define a square class model."""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """Square that implement from a rectangle."""
+
     def __init__(self, size, x=0, y=0, id=None):
+        """Define initalize a square with id y x and size set."""
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
+        """Return a size with a given width."""
         return self.width
 
     @size.setter
     def size(self, value):
+        """Write a size with a value typr and val error."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -21,6 +27,7 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
+        """Define an update args and kwargs."""
         if len(args) == 0:
             for lock, set_val in kwargs.items():
                 self.__setattr__(lock, set_val)
@@ -35,6 +42,7 @@ class Square(Rectangle):
             pass
 
     def to_dictionary(self):
+        """Return a dictionary with id x and size."""
         return {
             "id": self.id,
             "x": self.x,
@@ -43,5 +51,6 @@ class Square(Rectangle):
         }
 
     def __str__(self):
+        """Return a square value with the given stance."""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.width)
