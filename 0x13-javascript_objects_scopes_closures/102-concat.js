783bbdc5e9253ff2a2301f8ea605e40fcc69e451
Kidus <kingkidy50@gmail.com>
2024-03-12 18:08:03 -0700

    2000

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
-const files = require('fs');
 
-const firstFileContent = files.readFileSync(process.argv[2]).toString();
-const secondFileContent = files.readFileSync(process.argv[3]).toString();
-files.writeFileSync(process.argv[4], concatenatedContent);
+const fs = require('fs');
+
+if (process.argv.length !== 5) {
+  console.error('Usage: ./102-concat.js fileA fileB fileC');
+  process.exit(1);
+}
+
+const fileAContent = fs.readFileSync(process.argv[2], 'utf8');
+const fileBContent = fs.readFileSync(process.argv[3], 'utf8');
+const concatenatedContent = fileAContent + fileBContent;
+fs.writeFileSync(process.argv[4], concatenatedContent);
