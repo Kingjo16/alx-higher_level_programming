31c2f536192fd299d201d37ef24662337af83002
Kidus <kingkidy50@gmail.com>
2024-03-16 09:06:50 -0700

    new add

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Script that lists the states of the database."""
 
 import sys
 import MySQLdb
@@ -9,12 +10,11 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=database)
+    db = MySQLdb.connect(host="localhost", user=username, passwd=password,
+                         db=database, port=3306)
 
     cursordb = db.cursor()
-    cursordb.execute("SELECT * FROM states")
+    cursordb.execute("SELECT * FROM states ORDER BY id ASC")
 
     rows = cursordb.fetchall()
 
