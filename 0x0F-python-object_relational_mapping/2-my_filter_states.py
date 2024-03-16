da910940b8abcaf1b47f696c0f4e8ebd5d7308c5
Kidus <kingkidy50@gmail.com>
2024-03-16 09:59:08 -0700

    user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,13 +9,14 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    state_name = sys.argv[4]
 
     db = MySQLdb.connect(host="localhost", user=username, passwd=password,
                          db=database, port=3306)
 
     cursordb = db.cursor()
-    cursordb.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                     .format(sys.argv[4]))
+    cursordb.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+                     .format(state_name))
 
     rows = cursordb.fetchall()
 
