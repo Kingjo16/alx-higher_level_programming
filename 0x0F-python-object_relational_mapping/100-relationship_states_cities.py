1f41fffe57c09785c30794103e2022b77cd66b40
Kidus <kingkidy50@gmail.com>
2024-03-22 11:19:10 -0700

    new advance

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Creates the State name passed a new arg."""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name))
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    se = Session()
+    state_name = State(name='Louisiana')
+    se.add(state_name)
+    ins_news = se.query(State).filter_by(name='Louisiana').first()
+    print(ins_news.id)
+    se.commit()
