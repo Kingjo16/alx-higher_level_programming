5b401183bc915555a3dda7f3601f0b5b621fe139
Kidus <kingkidy50@gmail.com>
2024-03-22 10:59:20 -0700

    number 5

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,6 +3,8 @@
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
+from sqlalchemy.ext.declarative import declarative_base
+
 
 class City(Base):
     """City defining class."""
