1970b8c790b4dd8db34327ccf1acb60ce7c796c1
Kidus <kingkidy50@gmail.com>
2024-05-10 05:29:59 -0700

    new add

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,8 +2,8 @@
 // Read from file
 
 const fs_l = require('fs');
-fs_l.readFile(process.argv[2], 'utf-8',
-  function (err, data) {
+
+fs_l.readFile(process.argv[2], 'utf-8', function (err, data) {
     if (err) {
       console.log(err);
       return;
