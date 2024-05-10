66fd0b995c433c5116d9990403de532ad617986c
Kidus <kingkidy50@gmail.com>
2024-05-10 06:13:46 -0700

    advanced 1

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,21 @@
 #!/usr/bin/node
 
 const req = require('request');
-const id = process.argv[2];
-const addr = `https://swapi-api.alx-tools.com/api/films/${id}`;
-
-req.get(addr, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      req.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
-    }
+const addr = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+req(addr, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  req(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
